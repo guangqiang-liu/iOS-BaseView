035a900b7cda4ec16324d4a4e39a94edebd0dce3
刘光强 <liuguangqiang@qianbaocard.com>
2018-05-10 14:21:55 +0800

    重新整理BaseView组件项目

--- WLBaseView/ViewController.h
+++ WLBaseView/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "WLBaseViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : WLBaseViewController
 
 
 @end
--- WLBaseView/ViewController.m
+++ WLBaseView/ViewController.m
@@ -17,13 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
